10dc4034cf97708f52bd43f040fa214d5d7f325b
Usuário 1 <gustavolevorato0@gmail.com>
2016-09-26 22:14:13 -0300

    começando

--- main.py
+++ main.py
@@ -10,19 +10,32 @@ class MainHandler(webapp2.RequestHandler):
     <title>Awesome Blog!</title>
   </head>
   <body>
-    <form method="post">
-      <input type="text" name="nome">
-      <input type="submit">
-    </form>
+    Essa e a pagina principal!
+  </body>
+</html>
+      """
+    )
+  
+  def post(self):
+    numero = self.request.get("numero")
+    self.response.out.write(int(numero)**2)
+
+class LoginHandler(webapp2.RequestHandler):
+  def get(self):
+    self.response.out.write(
+      """
+<html>
+  <head>
+    <title>Awesome Blog!</title>
+  </head>
+  <body>
+    Essa e a pagina de login!
   </body>
 </html>
       """
     )
 
-  def post(self):
-    nome = self.request.get("nome")
-    self.response.out.write("Oi " + nome)
-
 app = webapp2.WSGIApplication([
-  ('/', MainHandler)
+  ('/', MainHandler),
+  ('/login', LoginHandler)
 ])
